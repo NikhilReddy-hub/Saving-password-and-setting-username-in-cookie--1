adbf7ba13e0ce8b57c4c22512c29212cbe78f5a6
Nikhil Reddy <alimilinikhilreddy@gmail.com>
2025-04-03 12:02:24 +0530

    done

--- script.js
+++ script.js
@@ -15,9 +15,22 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   // 1. Get the value of the 'count' cookie
+  let count = getCookie('count');
   // 2. If the cookie exists, increment the value and update the cookie
+  if (count) {
+    count = parseInt(count) +1
+  } else{
+    
+  
   // 3. If the cookie does not exist, create it and set the value to 1
+  
+    count++;
+  }
+  setCookie('count',count,7);
   // 4. Display the count on the webpage
-
-  // your code here
+  const countDisplay = document.createElement('div');
+  countDisplay.textContent = `Page visit count: ${count}`
+  countDisplay.style.fontSize = '20px';
+  countDisplay.style.margintop = '20px';
+  document.body.appendChild(countDisplay);
 });
